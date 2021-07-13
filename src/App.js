6335c87abd8e91e83ac8056005962a1462ed6a8b
Emily Read <emilyread01@gmail.com>
2021-07-13 08:45:31 -0700

    added API request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [poke, setPoke] = useState([]);
+
+   const fetchpoke=()=>{
+      fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response=>{
+        console.log("fetched")
+        return response.json();
+      }).then(response=>{
+          setPoke(response.results)
+      }).catch(err=> {console.log("error")})
+    }
+
+    return(
+      <div>
+        <h1> Pokemon </h1>
+        <button onClick={fetchpoke}>Fetch Pokemon</button>
+        {poke.length>0&& poke.map((poke, index)=>{
+          return(
+            <div key={index}>
+              <ul>
+                <li>{poke.name}</li>
+              </ul>
+            </div>
+          )
+        })}
+      </div>
+    )
+
+  
+  // return (
+  //   <div className="App">
+  //     <button onClick={FetchPoke}>Fetch Pokemon</button>
+      
+  //   </div>
+  // );
 }
 
 export default App;
